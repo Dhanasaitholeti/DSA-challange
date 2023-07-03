62b7c1e881b38d8b653ffba0285f4b8a6b330c5a
Dhanasai Tholeti <dhanasai5566@gmail.com>
2023-07-03 20:33:58 +0530

    added new problem

--- TwoSum/Js/TwosumNaive.js
+++ TwoSum/Js/TwosumNaive.js
@@ -1,8 +1,10 @@
-let arr = [2, 7, 11, 15];
+let arr = [3, 2, 4];
+let target = 6;
+console.log(solveproblem(arr, target));
 
 function solveproblem(nums, tar) {
   for (let i = 0; i < nums.length; i++) {
-    for (j = i; j < nums.length; j++) {
+    for (let j = i + 1; j < nums.length; j++) {
       if (nums[i] + nums[j] == tar) {
         return [i, j];
       }
