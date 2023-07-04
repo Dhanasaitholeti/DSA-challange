393f78b522c00278731f2996ef5b92e63c86296f
Dhanasai Tholeti <dhanasai5566@gmail.com>
2023-07-04 08:46:28 +0530

    solved the valid paranthesis problem in leetcode in cpp

--- validParanthesis/C++/validParanthesis.cpp
+++ validParanthesis/C++/validParanthesis.cpp
@@ -3,9 +3,34 @@
 #include<string>
 
 using namespace std;
+
+
+bool isValidParanthesis(string s){
+    stack<char> stk;
+    //we are having only these characters "({[]})" in the string so i companring directly
+    
+    for(char c:s){
+        if( c =='(' || c == '{' || c == '[' ){
+            stk.push(c);
+        }
+        else{
+            if(stk.empty()){
+                cout << "doing the false";
+                return false;
+            }
+            if( c ==']' && stk.top()=='[' || c =='}' && stk.top()=='{' || c ==')' && stk.top()=='(' ) stk.pop();
+
+            else return false;
+        }
+    }
+    cout << stk.empty();
+    return stk.empty();
+}
+
+
+
 int main(){
-    string str = "()[]({})";
-
-
+    string str = "()[]";   
+    isValidParanthesis(str);
     return 0;
 }
